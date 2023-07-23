90896c60f42033913ae719b5933825be5de845a5
mr1Michael <130656746+mr1Michael@users.noreply.github.com>
2023-07-23 12:14:15 +0200

    Update clingo_cats.py

--- clingo_cats.py
+++ clingo_cats.py
@@ -49,3 +49,22 @@ litter(dakota, 3).
 
 if __name__ == "__main__":
     main()
+
+"""Cats in Spring
+On a beautiful Spring day in the park, there were three female cats - Ruby, Spot and, Dakota - walking together 
+and gossiping about their humans. Up ahead, there were three male cats - Alfredo, Bock, and Cheddar - walking 
+together bragging about their conquests. When the two groups met, there were three cupids flying through the air. 
+Next thing you knew, there were three couples walking away in separate directions. The couples began talking and 
+soon found that they had amazing things in common. Well, long story short, we all know what happens to cats that 
+fall in love in the springtime, oh my what a racket! From this information and the following clues, can you 
+determine each females' male companion, her favorite activity and how many kittens - 1, 2, or 3 - she had in her l
+itter?
+
+- Alfredo chose the female who liked to chase a ball, but she was not Dakota.
+- Bock's companion liked to chase the laser light.
+- Ruby loved to cuddle up to her male for a long afternoon nap in the sun.
+- Dakota had two more kittens than Alfredo's companion.
+
+Write a Clingo program that returns a stable model for the puzzle above. Add an English sentence for each rule 
+of the program as an explanatory comment. If necessary, add more comments about your program and describe your 
+steps towards a solution."""
